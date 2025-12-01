ceebf3b9120cf919dfa9aa6f7047fb946be0d665
Meena-kumari88 <meenasahu8827@gmail.com>
2025-12-01 16:42:18 +0530

    get api for landing page

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const apiUrl = 'http://test-eventexpertv-5joh0p-8d631a-31-97-60-222.traefik.me' 
+// const apiUrl = import.meta.env.VITE_API_BASE_URL 
+const apiUrl = 'http://test-eventexpertv-5joh0p-8d631a-31-97-60-222.traefik.me';
 
 
 export const getLandingPage = async (token:string) => {
