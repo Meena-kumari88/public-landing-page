49b193f13054647b2a706e8279f816d606ccb81a
Meena-kumari88 <meenasahu8827@gmail.com>
2025-12-01 16:20:49 +0530

    get api for landing page

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -4,10 +4,6 @@ const apiUrl = import.meta.env.VITE_API_BASE_URL
 
 
 export const getLandingPage = async (token:string) => {
-    const response = await axios.get(`${apiUrl}/api/public/landingpage/${token}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
+    const response = await axios.get(`${apiUrl}/api/public/landingpage/${token}`);
     return response;
 };
